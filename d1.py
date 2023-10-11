8c38959d25523bdf255785d804ab4954fd83b892
aaryadahal <aaryadahal000@gmail.com>
2023-10-11 23:57:30 +0545

    Day 1 of Python

---
+++ d1.py
@@ -0,0 +1,35 @@
+''' Program to ask amount and credit score and reduce it by 20% if credit score is good 
+and reduce by 10% if credit score is bad. '''
+
+# Price=input("Enter price range:");
+# price=float(Price);
+# answer=input("Do you have good credit(yes/no)?");
+# if (answer.upper()=='YES'):
+#     has_good_credit=True;
+# else:
+#     has_good_credit=False;
+# if has_good_credit:
+#     actual_pay=price-(10/100)*price;
+#     credit_score="Good";
+# else:
+#     actual_pay=price-(20/100)*price;
+#     credit_score="Bad";
+# print(f"Since your credit score is {credit_score}");
+# print(f"The price to be paid is {actual_pay}");
+
+''' Write a program to ask a user for credit score and salary to decide if he can recieve loan or not.
+Note: User has to fulfill both the conditions. '''
+
+# credit_score = input("Enter your credit score:(1 to 10)")
+# salary = input("Enter your salary:")
+# if (float(credit_score) > 5) and (float(salary) > 35000):
+
+#     print("Congratulations! You are eligible recieve take loan from our bank.")
+# elif (float(credit_score) < 5):
+
+#     print(
+#         f"Sorry, unfortunately with a credit score of {credit_score}, you cannot recieve loan form our bank.")
+# else:
+#     print(
+#         f"Sorry, unfortunately with a salary of {salary}, you cannot recieve loan form our bank.")
+
