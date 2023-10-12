adcbb6df4964d5bac2cef92e2082560e0b95c823
aaryadahal <aaryadahal000@gmail.com>
2023-10-12 21:49:18 +0545

    day 1 of python

---
+++ d2.py
@@ -0,0 +1,40 @@
+''' A program to approve if the sixe of your word is ppossible or not
+word must be more than 3 letters and less than 50 letters.'''
+
+# name=input("Enter your name:");
+# if len(name)<3:
+#     print(f"{name} is too small.");
+# elif len(name)>50:
+#     print(f"{name} is too long."); 
+# else:
+#     print("Perfect name.");
+
+'''Ask user for weight. If it is in kg cpnvert to pounds and vice versa'''
+
+# weight = int(input('Weight:'));
+# unit = input('(L)bs or (K)g:');
+# if unit.upper() == "L":
+#     converted = weight * 0.45
+#     print(f"You are {converted} kilos");
+# else:
+#     converted = weight / 0.45
+#     print(f"You are {converted} lbs");
+
+'''car game'''
+
+# while True:
+#     command=input(">");
+#     if command.lower()== "help":
+#         print("start - to start the car");
+#         print("stop - to stop the car");
+#         print("quit - to exit");
+#     elif command.lower()== "start":
+#         print("car is moving.");
+#     elif command.lower() =="stop":
+#         print("car Stopped.");
+#     elif command.lower() =="quit":
+#         print("Program ends...");
+#         break;
+#     else:
+#         print("Didn't understand");
+# print("Thanks for playing!");
